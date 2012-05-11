9d64e7e60065fa36a8eaae73546c4b30bfa21645
SHIMODA Hiroshi <shimoda@clear-code.com>
2012-05-11 14:54:16 +0900

    Remove obsolete properties

--- content/direct-signature-editor/editor.js
+++ content/direct-signature-editor/editor.js
@@ -84,25 +84,6 @@ var DirectSignatureEditor = {
 				this.AccountManager.defaultAccount.defaultIdentity.key;
 	},
 
-	mIOService : Components.classes['@mozilla.org/network/io-service;1']
-		.getService(Components.interfaces.nsIIOService),
-
-	get fileHandler()
-	{
-		if (!this.mFileHandler)
-			this.mFileHandler = this.mIOService.getProtocolHandler('file')
-				.QueryInterface(Components.interfaces.nsIFileProtocolHandler);
-		return this.mFileHandler;
-	},
-
-	getFileWithPath : function(aPath)
-	{
-		var file = Components.classes['@mozilla.org/file/local;1']
-					.createInstance(Components.interfaces.nsILocalFile);
-		file.initWithPath(aPath);
-		return file;
-	},
-
 	init : function()
 	{
 		window.removeEventListener('DOMContentLoaded', this, false);
