9c34e718f7b7afbfb42fbbac4f9947b1734e83f6
YUKI Hiroshi <shimoda@clear-code.com>
2018-07-02 16:53:22 +0900

    Bump version to 0.4, and support Tb52-60

--- modules/prefs.js
+++ modules/prefs.js
@@ -2,9 +2,9 @@
  Preferences Library
 
  Usage:
-   var value = window['piro.sakura.ne.jp'].prefs.getPref('my.extension.pref');
-   window['piro.sakura.ne.jp'].prefs.setPref('my.extension.pref', true);
-   window['piro.sakura.ne.jp'].prefs.clearPref('my.extension.pref');
+   var value = prefs.getPref('my.extension.pref');
+   prefs.setPref('my.extension.pref', true);
+   prefs.clearPref('my.extension.pref');
    var listener = {
          domains : [
            'browser.tabs',
@@ -13,57 +13,40 @@
          observe : function(aSubject, aTopic, aData)
          {
            if (aTopic != 'nsPref:changed') return;
-           var value = window['piro.sakura.ne.jp'].prefs.getPref(aData);
+           var value = prefs.getPref(aData);
          }
        };
-   window['piro.sakura.ne.jp'].prefs.addPrefListener(listener);
-   window['piro.sakura.ne.jp'].prefs.removePrefListener(listener);
+   prefs.addPrefListener(listener);
+   prefs.removePrefListener(listener);
 
- license: The MIT License, Copyright (c) 2009-2010 SHIMODA "Piro" Hiroshi
-   http://github.com/piroor/fxaddonlibs/blob/master/license.txt
+   // utility
+   var store = prefs.createStore('extensions.someextension.');
+   // property name/key, default value
+   store.define('enabled', true);
+   // property name, default value, pref key (different to the name)
+   store.define('leftMargin', true, 'margin.left');
+   var enabled = store.enabled;
+   store.destroy(); // free the memory.
+
+ license: The MIT License, Copyright (c) 2009-2017 YUKI "Piro" Hiroshi
  original:
-   http://github.com/piroor/fxaddonlibs/blob/master/prefs.js
-   http://github.com/piroor/fxaddonlibs/blob/master/prefs.test.js
+   http://github.com/piroor/fxaddonlib-prefs
 */
 
-/* To work as a JS Code Module  */
-if (typeof window == 'undefined' ||
-	(window && typeof window.constructor == 'function')) {
-	this.EXPORTED_SYMBOLS = ['prefs'];
-
-	// If namespace.jsm is available, export symbols to the shared namespace.
-	// See: http://github.com/piroor/fxaddonlibs/blob/master/namespace.jsm
-	try {
-		let ns = {};
-		Components.utils.import('resource://my-modules/namespace.jsm', ns);
-		/* var */ window = ns.getNamespaceFor('piro.sakura.ne.jp');
-	}
-	catch(e) {
-		window = {};
-	}
-}
+var EXPORTED_SYMBOLS = ['prefs'];
+var prefs;
 
 (function() {
-	const currentRevision = 8;
-
-	if (!('piro.sakura.ne.jp' in window)) window['piro.sakura.ne.jp'] = {};
-
-	var loadedRevision = 'prefs' in window['piro.sakura.ne.jp'] ?
-			window['piro.sakura.ne.jp'].prefs.revision :
-			0 ;
-	if (loadedRevision && loadedRevision > currentRevision) {
-		return;
-	}
+	const currentRevision = 20;
 
 	const Cc = Components.classes;
 	const Ci = Components.interfaces;
 
-	window['piro.sakura.ne.jp'].prefs = {
+	prefs = {
 		revision : currentRevision,
 
-		Prefs : Cc['@mozilla.org/preferences;1']
-					.getService(Ci.nsIPrefBranch)
-					.QueryInterface(Ci.nsIPrefBranch2),
+		Prefs : Cc['@mozilla.org/preferences-service;1']
+					.getService(Ci.nsIPrefBranch),
 
 		DefaultPrefs : Cc['@mozilla.org/preferences-service;1']
 					.getService(Ci.nsIPrefService)
@@ -83,20 +66,34 @@ if (typeof window == 'undefined' ||
 			if (aInterface)
 				return aBranch.getComplexValue(aPrefstring, aInterface);
 
-			switch (type)
-			{
-				case aBranch.PREF_STRING:
-					return decodeURIComponent(escape(aBranch.getCharPref(aPrefstring)));
+			try {
+				switch (type)
+				{
+					case aBranch.PREF_STRING:
+						return decodeURIComponent(escape(aBranch.getCharPref(aPrefstring)));
 
-				case aBranch.PREF_INT:
-					return aBranch.getIntPref(aPrefstring);
+					case aBranch.PREF_INT:
+						return aBranch.getIntPref(aPrefstring);
 
-				case aBranch.PREF_BOOL:
-					return aBranch.getBoolPref(aPrefstring);
+					case aBranch.PREF_BOOL:
+						return aBranch.getBoolPref(aPrefstring);
 
-				case aBranch.PREF_INVALID:
-				default:
-					return null;
+					case aBranch.PREF_INVALID:
+					default:
+						return null;
+				}
+			} catch(e) {
+				// getXXXPref can raise an error if it is the default branch.
+				return null;
+			}
+		},
+
+		getLocalizedPref : function(aPrefstring)
+		{
+			try {
+				return this.getPref(aPrefstring, Ci.nsIPrefLocalizedString).data;
+			} catch(e) {
+				return this.getPref(aPrefstring);
 			}
 		},
 
@@ -105,19 +102,36 @@ if (typeof window == 'undefined' ||
 			return this.getPref(aPrefstring, this.DefaultPrefs, aInterface);
 		},
 	 
-		setPref : function(aPrefstring, aNewValue, aBranch) 
+		setPref : function(aPrefstring, aNewValue) 
 		{
-			aBranch = aBranch || this.Prefs;
+			var branch = this.Prefs;
+			var dataTypeInterface = null;
+			if (arguments.length > 2) {
+				for (let i = 2; i < arguments.length; i++)
+				{
+					let arg = arguments[i];
+					if (!arg)
+						continue;
+					if (arg instanceof Ci.nsIPrefBranch)
+						branch = arg;
+					else
+						dataTypeInterface = arg;
+				}
+			}
+			if (dataTypeInterface &&
+				aNewValue instanceof Ci.nsISupports) {
+				return branch.setComplexValue(aPrefstring, dataTypeInterface, aNewValue);
+			}
 			switch (typeof aNewValue)
 			{
 				case 'string':
-					return aBranch.setCharPref(aPrefstring, unescape(encodeURIComponent(aNewValue)));
+					return branch.setCharPref(aPrefstring, unescape(encodeURIComponent(aNewValue)));
 
 				case 'number':
-					return aBranch.setIntPref(aPrefstring, parseInt(aNewValue));
+					return branch.setIntPref(aPrefstring, parseInt(aNewValue));
 
 				default:
-					return aBranch.setBoolPref(aPrefstring, !!aNewValue);
+					return branch.setBoolPref(aPrefstring, !!aNewValue);
 			}
 		},
 
@@ -140,20 +154,26 @@ if (typeof window == 'undefined' ||
 	 
 		getChildren : function(aRoot, aBranch) 
 		{
-			return this.getDescendant(aRoot, aBranch)
-					.filter(function(aPrefstring) {
-						var name = aPrefstring.replace(aRoot, '');
-						if (name.charAt(0) == '.')
-							name = name.substring(1);
-						return name.indexOf('.') < 0;
+			aRoot = aRoot.replace(/\.$/, '');
+			var foundChildren = {};
+			var possibleChildren = [];
+			this.getDescendant(aRoot, aBranch)
+					.forEach(function(aPrefstring) {
+						var name = aPrefstring.replace(aRoot + '.', '');
+							let possibleChildKey = aRoot + '.' + name.split('.')[0];
+							if (possibleChildKey && !(possibleChildKey in foundChildren)) {
+								possibleChildren.push(possibleChildKey);
+								foundChildren[possibleChildKey] = true;
+							}
 					});
+			return possibleChildren.sort();
 		},
 	 
 		addPrefListener : function(aObserver) 
 		{
 			var domains = ('domains' in aObserver) ? aObserver.domains : [aObserver.domain] ;
 			try {
-				for each (var domain in domains)
+				for (var domain of domains)
 					this.Prefs.addObserver(domain, aObserver, false);
 			}
 			catch(e) {
@@ -164,15 +184,45 @@ if (typeof window == 'undefined' ||
 		{
 			var domains = ('domains' in aObserver) ? aObserver.domains : [aObserver.domain] ;
 			try {
-				for each (var domain in domains)
+				for (var domain of domains)
 					this.Prefs.removeObserver(domain, aObserver, false);
 			}
 			catch(e) {
 			}
+		},
+
+		createStore : function(aDomain)
+		{
+			var listener = {
+				domain : aDomain,
+				observe : function(aSubject, aTopic, aData) {
+					if (aTopic != 'nsPref:changed')
+						return;
+					var name = keyToName[aData];
+					store[name] = prefs.getPref(aData);
+				}
+			};
+			this.addPrefListener(listener);
+			var keyToName = {};
+			var base = aDomain.replace(/\.$/, '') + '.';
+			var store = {
+				define : function(aName, aValue, aKey) {
+					aKey = base + (aKey || aName);
+					prefs.setDefaultPref(aKey, aValue);
+					this[aName] = prefs.getPref(aKey);
+					keyToName[aKey] = aName;
+				},
+				destroy : function() {
+					prefs.removePrefListener(listener);
+					aDomain = undefined;
+					base = undefined;
+					listener = undefined;
+					keyToName = undefined;
+					store = undefined;
+				}
+			};
+			return store;
 		}
 	};
 })();
 
-if (window != this) { // work as a JS Code Module
-	this.prefs = window['piro.sakura.ne.jp'].prefs;
-}
